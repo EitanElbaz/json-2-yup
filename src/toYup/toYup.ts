4550daab382c53eac31135aafd12db2eb074ee81
Eitan Elbaz <eitan.elbaz@loan.co.uk>
2020-08-07 14:57:09 +0100

    number min max tests

--- src/toYup/toYup.ts
+++ src/toYup/toYup.ts
@@ -1,11 +1,15 @@
 import YupTypeSchema from 'src/types/YupTypeSchema';
 import StringTypeSchema from 'src/types/StringTypeSchema';
 import toYupString from 'src/toYup/toYupString';
+import toYupNumber from 'src/toYup/toYupNumber';
+import NumberTypeSchema from 'src/types/NumberTypeSchema';
 
 const toYup = (schema: YupTypeSchema) => {
     switch (schema.type) {
         case 'string':
             return toYupString(schema as StringTypeSchema);
+        case 'number':
+            return toYupNumber(schema as NumberTypeSchema);
     }
 };
 
