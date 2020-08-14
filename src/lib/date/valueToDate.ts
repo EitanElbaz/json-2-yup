5e30bd0ccb5724047e1cda241d6971a3c79a2268
Eitan Elbaz <eitan.elbaz@loan.co.uk>
2020-08-14 11:17:45 +0100

    allow date strings + tests

---
+++ src/lib/date/valueToDate.ts
@@ -0,0 +1,11 @@
+import { fromUnix } from 'src/lib/date/index';
+
+const valueToDate = (value: string | number): Date => {
+    if (Number.isInteger(value)) {
+        return fromUnix(value as number);
+    } else if (typeof value === 'string') {
+        return new Date(value);
+    }
+    return null;
+};
+export default valueToDate;
